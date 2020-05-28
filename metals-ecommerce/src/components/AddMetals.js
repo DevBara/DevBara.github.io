d71f1fa3c7853d8fc41b027bacf9ab5ec2ee707a
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-28 14:45:34 -0500

    component for admin to add new products

---
+++ metals-ecommerce/src/components/AddMetals.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+class AddMetals extends Component {
+    constructor(props){
+        super(props)
+
+        this.state={
+            name: '',
+            price: '',
+            quantity: ''
+        }
+    }
+
+    changeHandler = e => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    submitHandler = e => {
+        e.preventDefault()
+        console.log(this.state);
+
+        axios.post('metals_api/v1/metals',
+            this.state)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        
+    }
+
+
+
+    render() {
+		const { name, price, quantity } = this.state
+		return (
+			<div>
+				<form onSubmit={this.submitHandler}>
+					<div>
+						<input
+							type="text"
+							name="name"
+							value={name}
+							onChange={this.changeHandler}
+						/>
+					</div>
+					<div>
+						<input
+							type="text"
+							name="price"
+							value={price}
+							onChange={this.changeHandler}
+						/>
+					</div>
+					<div>
+						<input
+							type="text"
+							name="quantity"
+							value={quantity}
+							onChange={this.changeHandler}
+						/>
+					</div>
+					<button type="submit">Add Metal</button>
+				</form>
+			</div>
+		)
+	}
+}
+export default AddMetals;
+
+
+
+// name
+// price
+// quantity
