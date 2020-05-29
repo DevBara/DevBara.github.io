b927e22482946445823680b773f866c2c889cab7
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-29 13:35:05 -0500

    new components

---
+++ metals-ecommerce/src/components/SubmitReviewForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import {Form,Label,Control,Group}from 'react-bootstrap'
+import axios from 'axios'
+
+ 
+export default class SubmitReviewForm extends Component {
+    constructor(props){
+        super(props);
+
+        this.state={
+            reviews: [],
+            ratings: []
+        }
+    }
+
+
+    submitHandler = e => {
+        e.preventDefault()
+        console.log(this.state);
+
+        axios.post('metals_api/v1/metals/reviews',
+            this.state)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        
+    }
+    render() {
+        return (
+            <div>
+                <Form>
+                    <Form.Group controlId="exampleForm.ControlSelect1">
+                        <Form.Label>Select Rating</Form.Label>
+                        <Form.Control as="select">
+                        <option></option>
+                        <option>1 star</option>
+                        <option>2 stars</option>
+                        <option>3 stars</option>
+                        <option>4 stars</option>
+                        <option>5 stars</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Leave a Review:</Form.Label>
+                        <Form.Control as="textarea" rows="9" />
+                    </Form.Group>
+                </Form>
+            </div>
+        )
+    }
+}
