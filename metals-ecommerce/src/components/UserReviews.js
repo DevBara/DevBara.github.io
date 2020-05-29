c6aa28b0dc33ff3bde2f6c3ccf8100f69397848f
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-29 13:37:42 -0500

    added fetch ability

--- metals-ecommerce/src/components/UserReviews.js
+++ metals-ecommerce/src/components/UserReviews.js
@@ -1,30 +1,60 @@
-import React, {useState } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom';
 
 
-export default function UserReviews() {
 
-    const[reviews,setReviews] = useState(null);
+export default class UserReviews extends Component {
+    constructor(props){
+        super(props);
 
-    const apiUrl ='http://localhost:8090/metals_api/v1/reviews';
-
-    const fetchData= async () =>{
-        const response = await axios.get(apiUrl)
-
-        setReviews(response.data)
+        this.state={
+            comments: '',
+            ratings:[],
+            reviews:[]
+        }
     }
 
+    componentDidMount(){
+        this.setState({
+            isLoading:true});
 
-    return (
-        <div className="userApp">
-            <h1> User Reviews</h1>
-            <h2>Dont let us tell you how great we are, see from our loyal customers</h2>
+            fetch('http://localhost:8090/metals_api/v1/reviews')
+                .then(response => response.json())
+                .then(data => this.setState({
+                    reviews: data,
+                    
+            }))
+
+        }
+
+ 
+
+    render() {
+        const {reviews} = this.state;
+
+        const reviewsList=reviews.map(reviews => {
+            return <div key={reviews.id}>
+                <h4>Rating is:{reviews.rating} out of 5 stars</h4>
+                <div>{reviews.comments}</div>            
+            </div>
+
+        });
+        
+        return (
             <div>
-                <button className="reviewButton" onClick={fetchData}>See Reviews</button>
+                <h1 className="reviewsTitle"> Dont take our word for it, see what others have to say!</h1>
+                <h2 className="reviewsSubTitle">Customer Reviews</h2>
+                <div className="reviewsList">{reviewsList}</div>
+                <br></br>
+                <br></br>
+                <br></br>
+                
+
+                <div> 
+                    <NavLink className="button" to="/leavereview">Leave a Review</NavLink>
+                </div>
             </div>
-           
-            </div>
-         
-    );
-    
+        )
+    }
 }
+
