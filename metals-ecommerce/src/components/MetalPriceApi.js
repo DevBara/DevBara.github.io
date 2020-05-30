f929513dddaddd6a1f1732c86dabc817eb7864b8
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-30 16:49:17 -0500

    trying to render API

--- metals-ecommerce/src/components/MetalPriceApi.js
+++ metals-ecommerce/src/components/MetalPriceApi.js
@@ -1,47 +1,44 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+// import axios from 'axios'
 
-export default class MetalPriceApi extends Component {
+// export default class MetalPriceApi extends Component {
+//     constructor(props){
+//         super(props);
+
+//         this.state={
+//             ratesList: []
+//         }
+//     }
 
 
-    constructor(props){
-        super(props);
+//     componentDidMount(){
+//         axios.get('https://metals-api.com/api/latest?access_key=dmq34ceggue0sri34iokre5rxcppv5s9e983jn98ivi5jfg545asqtegn5r0&base=USD&symbols=XAU,XAG,XPD,XPT,XRH')
+//         .then(res => {
+//             const tickers=res.data.rates;
+//             console.log(tickers)
 
-        this.state ={
-           prices: '',
-           
-        };
+//             this.setState({
+//                 ratesList:res.data.rates
+//             });
+//         });
+//     }
 
-       
-    }
 
-    componentDidMount(){
-      
+//     render() {
 
-            fetch(`https://metals-api.com/api/latest?access_key=${process.env.REACT_APP_METALS}&base=USD&symbols=XAU,XAG,XPD,XPT,XRH`)
-                .then(response => response.json())
-                .then(data => this.setState({
-                    prices: data
-                    
-            }))
+//         const{ratesList} = this.state;
 
-        }
-        
-        render() {
-            const{prices} = this.state;
+//         const rates = ratesList.map(ratesList => {
+//             return <div key ={ratesList}>
+//                 <h1>{ratesList}</h1>
+//             </div>
+//         })
 
-            const priceTicker=prices.map(prices => {
-                return <div key ={prices.rates}>
-                   <div>{prices.rates}</div>          
-                </div>
-            });
-
-            return (
-                <div className="currencyTitle">
-                    {priceTicker}
-                 
-                </div>
+//         return (
+//             <div>
+//                 {rates}
                 
-            )
-        }  
-    }
-
+//             </div>
+//         )
+//     }
+// }
