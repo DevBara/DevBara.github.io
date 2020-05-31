900732c24f39b23f8ed90a48eaedcef672176b54
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-31 09:22:38 -0500

    attemp to make delete function work

--- metals-ecommerce/src/components/SubmitReviewForm.js
+++ metals-ecommerce/src/components/SubmitReviewForm.js
@@ -8,8 +8,8 @@ export default class SubmitReviewForm extends Component {
         super(props);
 
         this.state={
-            comments: [],
-            ratings: []
+            comments: '',
+            rating: ''
         }
     }
 
@@ -18,9 +18,9 @@ export default class SubmitReviewForm extends Component {
     }
 
     submitHandler = e => {
-        e.preventDefault()
+        window.alert('Review has been submitted')
 
-        axios.post('metals_api/v1/metals/reviews',
+        axios.post('metals_api/v1/reviews',
             this.state)
             .then(response => {
                 console.log(response)
@@ -30,15 +30,49 @@ export default class SubmitReviewForm extends Component {
             })
         
     }
+
+    async componentDidMount() {
+        if (this.props.match.params.id !== 'new') {
+          const group = await (await fetch(`/metals_api/v1/reviews/${this.props.match.params.id}`)).json();
+          this.setState({item: group});
+        }
+      }
+
+    async remove(id) {
+        await fetch(`/metals_api/v1/reviews`, {
+          method: 'DELETE',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        }).then(() => {
+          let updatedReviews = [...this.state.reviews].filter(i => i.id !== id);
+          this.setState({currencies: updatedReviews});
+        });
+      }
+
+    // deleteHandler = e =>{
+    //     e.preventDefault()
+    //     console.log(this.state)
+
+    //     axios.delete(`metals_api/v1/reviews/${id}`, this.state)
+    //         .then(response =>{
+    //             console.log(response);
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //         })
+    //     }
     render() {
 
-        const{comments, ratings} = this.state
+        const{comments, rating} = this.state
         return (
-            <div>
-                <form>
+        <div>
+                <form onSubmit={this.submitHandler}> 
                     <div>
-                        <p>Select Rating</p>
-                        <select>
+                        <label for="rating">Select Rating</label>
+
+                        <select name="rating" onChange={this.changeHandler} value={rating}>
                         <option value="0">-</option>
                             <option value="1">1 star</option>
                             <option value="2">2 stars</option>
@@ -46,7 +80,16 @@ export default class SubmitReviewForm extends Component {
                             <option value="4">4 stars</option>
                         </select>
                     </div>
-                    <textarea rows="9" cols="70" placeholder="Type Review Here..."></textarea>
+                    <textarea
+                        rows="9" cols="70" 
+                        placeholder="Type Review Here..." 
+                        value={comments}
+                        onChange={this.changeHandler}
+                        name="comments"
+                    >
+                    </textarea>
+                    <button type="submit">Submit Review</button>
+                    <button type="button" onClick={() => this.remove()}>Remove Review</button>
                 </form>
             </div>
         )
