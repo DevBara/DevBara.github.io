231d5769bfa54cf612278186f8c153466f33f3fe
DevBara <61251493+DevBara@users.noreply.github.com>
2020-06-02 10:17:32 -0500

    edits to Footer

--- metals-ecommerce/src/components/Footer.js
+++ metals-ecommerce/src/components/Footer.js
@@ -6,10 +6,7 @@ import { Button}  from 'reactstrap';
 function Footer(){
 
 return (<div>
-    <h1>Contact us at: </h1>
-    <p>email: matreshka@gmail.com </p>
-    <p>tel:718-458-2317 </p>
-    <p>Address: Matreshka Inc. 17 5th Avenue, 3rd Floor, New York, NY 10017 </p>
+    <h1>Dont Forget to Subscribe</h1>
     <Button size="sm" color="primary">Subscribe</Button> 
 </div>)
 }
